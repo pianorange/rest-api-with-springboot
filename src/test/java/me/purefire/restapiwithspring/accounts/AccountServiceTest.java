4c372ee17444f0d70fed6d84e57f80f3cd25c5a5
pianorange123 <pianorange123@gmail.com>
2019-01-08 22:16:39 +0900

    add Expected Exception Test

--- src/test/java/me/purefire/restapiwithspring/accounts/AccountServiceTest.java
+++ src/test/java/me/purefire/restapiwithspring/accounts/AccountServiceTest.java
@@ -1,22 +1,31 @@
 package me.purefire.restapiwithspring.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    //Junit 제공 Rule 중하나
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     //@Service 로 Bean 등록안하면 Autowired 안됨
     @Autowired
@@ -44,8 +53,32 @@ public class AccountServiceTest {
 
         //Then
         assertThat(userDetails.getPassword()).isEqualTo(pass);
-        assertThat(userDetails.getUsername()).isEqualTo(username);
+    }
 
+    //@Test(expected = UsernameNotFoundException.class)
+    @Test
+    public void findByUsernameFail() {
+        String username = "random@email.com";
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed. expected throws UserFoundException");
+        } catch (UsernameNotFoundException e) {
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }
+
+    /**
+     * JUnit 에서 제공하는 ExpectedException 을 사용하여 테스트하는법
+     */
+    @Test
+    public void findByUsernameFail2() {
+        //Expected 이므로 예상되는 예외와 메세지 확인사항 선언
+        String username = "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        //When
+        accountService.loadUserByUsername(username);
     }
 
 }
